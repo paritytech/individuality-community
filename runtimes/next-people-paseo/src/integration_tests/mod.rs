68ed4ab9617283ea68ed81996ee0149cd06a6b55
Andrzej Sulkowski <andrzej@parity.io>
2026-09-15 15:47:56 +0300

    rmv xcm barrier

--- runtimes/next-people-paseo/src/integration_tests/mod.rs
+++ runtimes/next-people-paseo/src/integration_tests/mod.rs
@@ -88,7 +88,6 @@ mod score_game_person_flow;
 mod statement_allowance;
 mod transaction_era;
 mod tx_payment_external_asset;
-mod xcm_barrier;
 
 type VrfSecret = <Crypto as GenerateVerifiable>::Secret;
 
